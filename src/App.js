9f84c142865e6aaa09b5be1ef49ada5714e2be06
Constanza Rojas <cons.rojasb@duocuc.cl>
2025-10-23 00:22:00 -0300

    arreglando errores

--- src/App.js
+++ src/App.js
@@ -50,9 +50,7 @@ export default function App() {
           </div>
         </div>
       </nav>
-jhdfkjhkngkdfngv
-kdsnvknfkjgfndsk
-gnjkdsndkvgb
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
